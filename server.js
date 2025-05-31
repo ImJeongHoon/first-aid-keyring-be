070a260b7c1cd7c7552b1784377afad75b47d2c0
ImJeongHoon <ijh701016@naver.com>
2025-06-01 06:19:58 +0900

    port code edit

--- server.js
+++ server.js
@@ -30,8 +30,9 @@ app.use("/api/users", userRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(5000, () => {
-      console.log("✅ Server running on http://localhost:5000");
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`✅ Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error(err));
