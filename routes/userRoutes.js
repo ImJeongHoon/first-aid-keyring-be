b6cca79b1bbaf0f906be21310e8d64f3db6dec79
ImJeongHoon <ijh701016@naver.com>
2025-06-01 07:47:29 +0900

    edit: before server.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,46 @@
 const express = require("express");
-const router = express.Router();
-const {
-  register,
-  login,
-  updateProfile,
-  getPublicProfile,
-  getMyProfile,
-} = require("../controllers/userController");
-const authMiddleware = require("../middleware/authMiddleware");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
 
-router.get("/:email", getPublicProfile); // 누구나 조회 가능
-router.post("/register", register);
-router.post("/login", login);
-router.put("/profile", authMiddleware, updateProfile);
-router.get("/profile", authMiddleware, getMyProfile); // ✅ 이거 추가
+dotenv.config();
 
-module.exports = router;
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://first-aid-keyring.vercel.app",
+  "https://first-aid-keyring-be.onrender.com/",
+];
+
+const app = express();
+app.use(
+  cors({
+    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  next();
+});
+
+app.use("/api/users", userRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`✅ Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
