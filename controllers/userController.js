9d949fb93f7c32ab4235e68cd6d662c15adbea6d
ImJeongHoon <ijh701016@naver.com>
2025-06-01 06:00:29 +0900

    3

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,18 +4,21 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, name } = req.body;
+
+    console.log("📥 회원가입 시도:", req.body);
 
     const existing = await User.findOne({ email });
     if (existing)
       return res.status(400).json({ message: "email already exists" });
 
     const hashed = await bcrypt.hash(password, 10);
-    const newUser = new User({ email, password: hashed });
+    const newUser = new User({ email, password: hashed, name }); // name 포함
     await newUser.save();
 
     res.status(201).json({ message: "User registered" });
   } catch (err) {
+    console.error("❌ 회원가입 에러:", err);
     res.status(500).json({ message: "Server error" });
   }
 };
@@ -77,6 +80,7 @@ exports.getPublicProfile = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
 exports.getMyProfile = async (req, res) => {
   try {
     const user = await User.findOne({ email: req.user.email }).select(
