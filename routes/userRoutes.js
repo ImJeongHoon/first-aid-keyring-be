82229ae8a8f1db379944cac970d2accff3015b90
ImJeongHoon <ijh701016@naver.com>
2025-06-01 12:08:44 +0900

    fix: regex error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,11 +9,11 @@ const {
 } = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/:email", getPublicProfile); // 누구나 조회 가능
 router.post("/register", register);
 router.post("/login", login);
 router.put("/profile", authMiddleware, updateProfile);
 router.get("/profile", authMiddleware, getMyProfile); // ✅ 이거 추가
+router.get("/:email", getPublicProfile); // 누구나 조회 가능
 
 module.exports = router;
 //
